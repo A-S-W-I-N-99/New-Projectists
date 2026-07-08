71bf45565cfa9e9a0d1f6cebdd8441c631fd26a4
devasenthil777-glitch <devasenthil777@gmail.com>
2026-07-08 21:05:35 +0530

    Added Fee Structure Management UI

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import TeacherLogin from "./pages/Login/TeacherLogin";
 import TeacherDashboard from "./pages/Dashboard/TeacherDashboard";
 import CollegeSettingsDashboard from "./pages/college-settings/CollegeSettingsDashboard";
 import NotificationCenter from "./pages/notification-center/NotificationCenter";
+import FeeStructureManagement from "./pages/fee-structure-management/FeeStructureManagement";
 function App() {
   return (
     <Routes>
@@ -13,6 +14,7 @@ function App() {
       <Route path="/dashboard" element={<TeacherDashboard />} />
       <Route path="/college-settings" element={<CollegeSettingsDashboard />}/>
       <Route path="/notification-center" element={<NotificationCenter />} />
+      <Route path="/fee-structure-management" element={<FeeStructureManagement />}/>
     </Routes>
   );
 }

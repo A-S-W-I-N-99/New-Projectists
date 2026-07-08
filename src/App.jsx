911f579203c3bbf26312f8876d29dce5e08798aa
devasenthil777-glitch <devasenthil777@gmail.com>
2026-07-08 19:55:05 +0530

    Added College Settings Dashboard UI

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import StudentLogin from "./pages/Login/StudentLogin";
 import TeacherLogin from "./pages/Login/TeacherLogin";
 import TeacherDashboard from "./pages/Dashboard/TeacherDashboard";
+import CollegeSettingsDashboard from "./pages/college-settings/CollegeSettingsDashboard";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Route path="/" element={<StudentLogin />} />
       <Route path="/teacher-login" element={<TeacherLogin />} />
       <Route path="/dashboard" element={<TeacherDashboard />} />
+      <Route path="/college-settings" element={<CollegeSettingsDashboard />}/>
     </Routes>
   );
 }
